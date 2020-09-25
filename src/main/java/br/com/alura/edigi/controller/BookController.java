f1cf0d863bbfc4baec6e66b3e8950545336883ee
Geovani Granieri <geovani.hilario@outlook.com>
2020-09-25 19:36:13 -0300

    refactor: better use of Java8 Optional

--- src/main/java/br/com/alura/edigi/controller/BookController.java
+++ src/main/java/br/com/alura/edigi/controller/BookController.java
@@ -9,7 +9,7 @@ import br.com.alura.edigi.repository.BookRepository;
 import br.com.alura.edigi.repository.CategoryRepository;
 
 public class BookController {
-   
+
     private BookRepository bookRepository;
     private AuthorRepository authorRepository;
 	private CategoryRepository categoryRepository;
@@ -29,16 +29,13 @@ public class BookController {
     }
 
     public List<Book> findByTitle(String title){
-        
+
         if( title.isEmpty() || title == null || title.length() < 2)
             throw new IllegalArgumentException("A busca por livro deve conter pelo menos 2 caracteres");
-        
+
         Optional<List<Book>> searchResult = bookRepository.findByTitle(title);
 
-        if(searchResult.isEmpty())
-            throw new IllegalArgumentException("Nenhum livro encontrado com essse título");
-
-        return searchResult.get();
+        return searchResult.orElseThrow( () -> new IllegalArgumentException("Nenhum livro encontrado com esse título"));
     }
 
 }
